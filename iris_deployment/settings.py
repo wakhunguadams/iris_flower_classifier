27cedce60041333d983c09e0b3a4c331a80e2d07
wakhunguadams <99601572+wakhunguadams@users.noreply.github.com>
2023-06-14 16:44:06 +0300

    Update settings.py
    security key =drive

--- iris_deployment/settings.py
+++ iris_deployment/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_bxgm^yz3vc$j1x6qwf57eq%%&hc60jukj3!*+!$-4mdd9(t@^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
